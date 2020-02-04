c1454760527f190542a3d10227a62b1bb5e6703c
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-02-04 15:54:56 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
-# code here!
+class School
+  attr_reader :roster, :school_name
+  def initialize(school_name, roster = {})
+    @school_name = school_name
+    @roster = roster
+  end 
+  def add_student(name, grade)
+    name_array = []
+    name_array << name
+    if @roster.empty?
+    @roster[grade] = name_array
+  else
+    if @roster.key?(grade)
+      
+  end
+  end
+end
